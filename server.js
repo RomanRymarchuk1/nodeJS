f3ebe0ed58384ccbf4b36672acaebf65fb385004
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2023-04-05 16:04:15 +0300

    add/ lesson 16

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const Post = require("./models/post");
 const Contact = require("./models/contact");
-const { log } = require("console");
-
+const methodOverride = require("method-override");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -30,6 +29,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static("styles"));
 
+app.use(methodOverride("_method"));
+
 app.get("/", (req, res) => {
    const title = "Home";
    res.render(createPath("index"), { title });
@@ -49,6 +50,36 @@ app.get("/posts/:id", (req, res) => {
    const title = "Post";
    Post.findById(req.params.id)
       .then((post) => res.render(createPath("post"), { post, title }))
+      .catch((err) => {
+         console.log(err);
+         res.render(createPath("error"), { title: "error" });
+      });
+});
+
+app.get("/edit/:id", (req, res) => {
+   const title = "Edit Post";
+   Post.findById(req.params.id)
+      .then((post) => res.render(createPath("edit-post"), { post, title }))
+      .catch((err) => {
+         console.log(err);
+         res.render(createPath("error"), { title: "error" });
+      });
+});
+
+app.put("/edit/:id", (req, res) => {
+   const { title, author, text } = req.body;
+   const { id } = req.params;
+   Post.findByIdAndUpdate(id, { title, author, text })
+      .then((post) => res.redirect(`/posts/${id}`))
+      .catch((err) => {
+         console.log(err);
+         res.render(createPath("error"), { title: "error" });
+      });
+});
+
+app.delete("/posts/:id", (req, res) => {
+   Post.findByIdAndDelete(req.params.id)
+      .then((result) => res.sendStatus(200))
       .catch((err) => {
          console.log(err);
          res.render(createPath("error"), { title: "error" });
